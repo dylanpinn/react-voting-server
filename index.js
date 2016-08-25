84eb1af84a3c1b0dcb6471807723a72a162a1cb3
Dylan Pinn <dylan@arcadiandigital.com.au>
2016-08-25 21:12:48 +1000

    Receiving Remote Redux Actions

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import makeStore from './src/store';
 import startServer from './src/server';
 
+const startEntries = require('./entries.json');
+
 export const store = makeStore();
 startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: startEntries,
+});
+store.dispatch({ type: 'NEXT' });
